7d92c1f9f6be4227a407437987418f8b99908785
Elie Prudhomme <elie.prudhomme@bluemind.net>
2021-02-26 17:08:59 +0100

    Update dependency version & build process

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,17 +1,15 @@
-import alias from 'rollup-plugin-alias';
+import alias from '@rollup/plugin-alias';
 import vue from 'rollup-plugin-vue';
-import buble from 'rollup-plugin-buble';
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import buble from '@rollup/plugin-buble';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  name: 'Multipane',
   input: 'src/index.js',
   output: {
     file: 'dist/vue-multipane.js',
     format: 'umd',
   },
-  sourcemaps: true,
   plugins: [
     alias({
       '@': './',
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,21 +1,16 @@
 const mix = require('laravel-mix');
-const mixEnv = require('laravel-mix-environments');
 const argv = require('yargs').argv;
+const path = require('path')
 
-// Add absolute path @ resolver.
-mix.options({
-  webpackConfig: {
-    resolve: {
-      alias: {
-        '@': path.resolve('./'),
-      },
+mix.webpackConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve('./'),
     },
   },
-});
+})
 
-if (argv.env.entry=='src') {
-
-  // Main script file
+if (argv.env === 'src') {
   mix
     .js(
       'src/index.js',
@@ -37,36 +32,31 @@ if (argv.env.entry=='src') {
     });
 }
 
-if (argv.env.entry=='demo') {
+if (argv.env === 'demo') {
 
   // Demo files
   mix
     .js('demo/src/main.js', 'demo/main.js')
-    .copy('demo/src/index.html', 'demo/index.html');
+    .copy('demo/src/index.html', 'demo/index.html')
+    .vue();
 
-  if (mix.inDevelopment()) {
+  if (process.env.NODE_ENV === 'development') {
+    console.log('and here')
 
-    // Dev Server
     const port = 8012;
     const publicPath = `http://localhost:${port}/`;
 
-    mix.options({
-      webpackConfig: {
-        output: {
-          publicPath: publicPath
-        },
-        devServer: {
-          port: port,
-          publicPath: publicPath,
-          public: `localhost:${port}`,
-          contentBase: './demo/',
-          noInfo: false,
-          quiet: false,
-          open: true,
-          overlay: {
-            warnings: true,
-            errors: true
-          }
+    mix.webpackConfig({
+      output: {
+        publicPath: publicPath
+      },
+      devServer: {
+        port: port,
+        public: `localhost:${port}`,
+        open: true,
+        overlay: {
+          warnings: true,
+          errors: true
         }
       }
     });
